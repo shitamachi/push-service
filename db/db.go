4ba7b2df222001e728a04283a0dadc5fda9e1b78
shitamachi <guolake@gmail.com>
2021-05-27 11:12:19 +0800

    use sql connection pool

--- db/db.go
+++ db/db.go
@@ -1,21 +1,32 @@
 package db
 
 import (
+	"entgo.io/ent/dialect/sql"
 	"fmt"
 	"github.com/shitamachi/push-service/config"
 	"github.com/shitamachi/push-service/ent"
+	"time"
 )
 
 var Client *ent.Client
 
 func InitDB() (*ent.Client, error) {
-	client, err := ent.Open("mysql",
+	driver, err := sql.Open(
+		"mysql",
 		//username:password.@tcp(127.0.0.1:3306)/db_name?checkConnLiveness=false&loc=Local&parseTime=true&readTimeout=1s&timeout=3s&writeTimeout=1s
-		getDSN(),
-		ent.Debug(),
-	)
+		getDSN())
 	if err != nil {
 		return nil, err
+	}
+
+	db := driver.DB()
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxLifetime(time.Hour)
+
+	client := ent.NewClient(ent.Driver(driver))
+	if client == nil {
+		return nil, fmt.Errorf("got client but is nil")
 	}
 
 	Client = client
